dd41e3747f795d1924e009afeeddba12d2411e33
doctorio228 <kukhtin.a@list.ru>
2024-07-24 15:06:03 +0300

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,9 +1,9 @@
 my_dict = {'Саша': 2022, 'Настя': 2020, 'Валя': 2019}
+print(my_dict)
 print(my_dict['Саша'])
-my_dict['Вася'] = 2016
-print(my_dict['Вася'])
+print(my_dict.get('Вася', "такого имени в словаре нету"))
 my_dict.update ({'Соня': 2011, 'Егор': 2013})
-del my_dict['Вася']
+del my_dict['Настя']
 print(my_dict)
 my_set = {1,3,2,4,2,3,1,4,'Man'}
 print(my_set)
